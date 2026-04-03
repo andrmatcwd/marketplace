69642fa9d8a12a27bf700e088d2116ad2a578103
Andrii Matchak <andrmatcwd@Andriis-MacBook-Air.local>
2026-04-03 22:09:15 +0300

    Initial commit

---
+++ Marketplace.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Marketplace.Web.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
