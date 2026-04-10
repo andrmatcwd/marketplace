1b7ef6c7bf712c203511faf9b863a90bd9f93102
andrmatcwd <andrmatcwd1221@gmail.com>
2026-04-10 15:07:12 +0300

    Change_base_filter_model

--- Marketplace.Web/Models/Common/BaseFilter.cs
+++ Marketplace.Web/Models/Common/BaseFilter.cs
@@ -4,6 +4,11 @@ namespace Marketplace.Web.Models.Common;
 
 public class BaseFilter
 {
+    public string? SearchTerm { get; set; }
+
+    public double? RatingFrom { get; set; }
+    public string SortBy { get; set; } = "newest";
+
     public int Page { get; set; } = 1;
     public int PageSize { get; set; } = 20;
 }
